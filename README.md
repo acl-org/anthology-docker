6c85da6a5636de2d0f22a9667e8f9204956bdbda
Martín Villalba <villalba@coli.uni-saarland.de>
2018-03-26 19:34:05 +0200

    Tweaked the startup a bit. Connection to the database is still spiffy.

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # anthology-docker
 Official source for Docker configurations, images, and examples of Dockerfiles for the ACL Anthology and projects
+# ACL Anthology Docker image
+
+This project aims to provide a Docker image of the ACL Anthology.
+
+The process for running the ACL Anthology as a Docker image consists of 3 steps:
+
+1. Set your configuration options
+2. Allocate data resources
+3. Run the Docker image
+
+## Step 1: set your configuration options
+
+The file `params.cfg` contains a list of variables that you need to set.
+These variables are:
+  * Database host: IP address of your database
+  * Database name: name of your database
+  * Username: the database user
+  * Password: the password for the database user
+
+## Step 2: allocate data resources
+
+Data for the Anthology is provided as a collection of PDF files and a 
+database. The Docker image includes scripts that periodically check for
+updates and download new data, which will be downloaded to your local server.
+
+Currently, the Anthology contains around 16 GiB worth of PDFs. These papers can 
+be stored in any directory you want, as long as Docker has access to it. This
+directory is referred to as `your_paper_dir` in Step 3.
+
+The database will be regenerated periodically. Make sure that the database user
+has write permissions.
+
+## Step 3: Run docker
+
+Start your Docker container the following way:
+
+```
+docker run <container_name> --env-file ./params.env -p 8080:80 \
+       -v /var/papers:your_paper_dir
+```
+
+
+# License
+This software is provided under the [JSON License](http://www.json.org/license.html).
--- startup.sh
+++ startup.sh
@@ -2,6 +2,7 @@
 
 SOLR_CONFIG=/home/acl/jetty/solr/blacklight-core/conf/data-config.xml
 JETTY_CONFIG=/home/acl/jetty/etc/jetty.xml
+DATABASE_CONFIG=/home/acl/config/database.yml
 
 # Update username, password, and database for Solr and Jetty in its config files
 # This script changes the 4th line to the content on the next line. Please note:
@@ -10,22 +11,58 @@ JETTY_CONFIG=/home/acl/jetty/etc/jetty.xml
 sed -i "4c\
 \              url=\"jdbc:postgresql://${PGSQL_HOST}/${PGSQL_DATABASE}\"" ${SOLR_CONFIG}
 sed -i "5c\
-\              user=\"${PGSQL_USER}\"/" ${SOLR_CONFIG}
+\              user=\"${PGSQL_USER}\"" ${SOLR_CONFIG}
 sed -i "6c\
-\              password=\"${PGSQL_PASS}\"/>" i${SOLR_CONFIG}
+\              password=\"${PGSQL_PASS}\"/>" ${SOLR_CONFIG}
 
 # Now, we change the IP that Jetty will use to 'localhost'
 sed -i "80c\
 \                                 <Arg>127.0.0.1</Arg>" ${JETTY_CONFIG}
 
+# Same for the database info
+sed -i "57c\
+\  database: ${PGSQL_DATABASE}" ${DATABASE_CONFIG}
+sed -i "58c\
+\  host: ${PGSQL_HOST}" ${DATABASE_CONFIG}
+sed -i "61c\
+\  username: ${PGSQL_USER}" ${DATABASE_CONFIG}
+sed -i "62c\
+\  password: ${PGSQL_PASS}" ${DATABASE_CONFIG}
+
+# Pull the latest version of the code - this should not overwrite the files
+# we've just edited.
+cd /home/acl
+git pull
+
+ping -c 3 134.96.104.204
+ping -c 3 ${PGSQL_HOST}
+
 # Start the services
-java -jar /home/acl/jetty/start.jar &
+cd /home/acl/jetty
+java -jar start.jar &
 cd /home/acl
 rails server -p 80 &
 
-# Beginning of an infinite loop
-# 1. Download a dump of the database, and overwrite the current one
-# 2. Download the new list of available PDFs
-# 3. Download missing PDFs, if any
-# 4. Generate new bib files?
-# Repeat until the heath death of the universe
+# At startup time, the server will perform the following tasks:
+#  - Rebuild the database
+#  - Download missing PDFs (if any)
+#  - Export all papers to Bibtex and similar formats
+# These tasks can take as long as they want, while the server should be running
+# fine in the meantime
+
+# Step 1: rebuild the database
+
+
+# Step 2: Re-export the papers
+cd /home/acl
+for file in import/*xml
+do
+      volume=`grep '<volume id' $file | sed 's/.*volume id=.\(.*\).>.*/\1/'`
+      for paper in `grep "paper id" $file | sed 's/.*id=.\([0-9]\+\).*/\1/'`
+      do
+              rake export:all_papers[${volume}-${paper}]
+      done
+done
+
+
+# Step 3: Re-download the PDFs
